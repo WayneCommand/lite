0d7ee4a0d6fe1a4319c9059abf07b6506b63dfdb
shenlanAZ <shenlanluck@gmail.com>
2021-09-13 15:24:56 +0800

    initial

---
+++ src/test/java/org/example/AppTest.java
@@ -0,0 +1,20 @@
+package org.example;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.platform.commons.annotation.Testable;
+
+/**
+ * Unit test for simple App.
+ */
+@Testable
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue() {
+        Assertions.assertTrue(true);
+    }
+}
